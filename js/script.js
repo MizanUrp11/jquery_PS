06df850e0872d8d31361fb3d65a14073db1ac41c
MizanUrp11 <mrn1105009@gmail.com>
2020-07-19 22:33:12 +0600

    jQuery problem solving

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  //Go to Top
+  $("a[href='#top']").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  //Disable right click menu option
+  $(document).bind("contextmenu", function () {
+    return false;
+  });
+
+  //Disable/enable the form submit button
+
+  $('#accept').click(function () { 
+      if($('#submitBtn').is(':disabled')){
+          $('#submitBtn').removeAttr('disabled')
+      }else{
+        $('#submitBtn').attr('disabled','disabled');
+      }
+   })
+});
